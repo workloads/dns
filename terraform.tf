61c1f40d3765123d7fd3bead60faf5d8f4505a3e
Kerim Satirli <k.satirli@gmail.com>
2022-10-01 10:40:53 +0200

    bumps AWS provider from `4.32.0` to `4.33.0`

--- terraform.tf
+++ terraform.tf
@@ -12,10 +12,10 @@ terraform {
 
   # see https://developer.hashicorp.com/terraform/language/settings#specifying-provider-requirements
   required_providers {
-    # see https://registry.terraform.io/providers/hashicorp/aws/4.32.0/
+    # see https://registry.terraform.io/providers/hashicorp/aws/4.33.0/
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.32.0, < 5.0.0"
+      version = ">= 4.33.0, < 5.0.0"
     }
 
     # see https://registry.terraform.io/providers/hashicorp/hcp/0.45.0/
