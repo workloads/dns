8958dbf664966e709626ec095891021ad8131e24
Kerim Satirli <k.satirli@gmail.com>
2022-10-08 14:02:41 +0200

    bumps AWS Provider from `4.33.0` to `4.34.0` and Gandi Provider from `2.1.0` to `2.2.0`

--- terraform.tf
+++ terraform.tf
@@ -12,10 +12,10 @@ terraform {
 
   # see https://developer.hashicorp.com/terraform/language/settings#specifying-provider-requirements
   required_providers {
-    # see https://registry.terraform.io/providers/hashicorp/aws/4.33.0/
+    # see https://registry.terraform.io/providers/hashicorp/aws/4.34.0/
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.33.0, < 5.0.0"
+      version = ">= 4.34.0, < 5.0.0"
     }
 
     # see https://registry.terraform.io/providers/hashicorp/hcp/0.45.0/
@@ -24,10 +24,10 @@ terraform {
       version = ">= 0.45.0, < 1.0.0"
     }
 
-    # see https://registry.terraform.io/providers/go-gandi/gandi/2.1.0
+    # see https://registry.terraform.io/providers/go-gandi/gandi/2.2.0
     gandi = {
       source  = "go-gandi/gandi"
-      version = ">= 2.1.0, < 3.0.0"
+      version = ">= 2.2.0, < 3.0.0"
     }
   }
 
