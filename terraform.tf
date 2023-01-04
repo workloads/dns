11a63831718124c3e41c030404db370fa44c2412
Kerim Satirli <k.satirli@gmail.com>
2023-01-04 16:11:30 +0100

    bumps AWS provider from `4.45.0` to `4.48.0` and HCP provider from `0.50.0` to `0.52.0`

--- terraform.tf
+++ terraform.tf
@@ -12,22 +12,22 @@ terraform {
 
   # see https://developer.hashicorp.com/terraform/language/settings#specifying-provider-requirements
   required_providers {
-    # see https://registry.terraform.io/providers/hashicorp/aws/4.45.0/
+    # see https://registry.terraform.io/providers/hashicorp/aws/4.48.0/
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.45.0, < 5.0.0"
+      version = ">= 4.48.0, < 5.0.0"
     }
 
-    # see https://registry.terraform.io/providers/hashicorp/hcp/0.50.0/
+    # see https://registry.terraform.io/providers/hashicorp/hcp/0.52.0/
     hcp = {
       source  = "hashicorp/hcp"
-      version = ">= 0.50.0, < 1.0.0"
+      version = ">= 0.52.0, < 1.0.0"
     }
 
-    # see https://registry.terraform.io/providers/go-gandi/gandi/2.2.0
+    # see https://registry.terraform.io/providers/go-gandi/gandi/2.2.2
     gandi = {
       source  = "go-gandi/gandi"
-      version = ">= 2.2.0, < 3.0.0"
+      version = ">= 2.2.2, < 3.0.0"
     }
   }
 
