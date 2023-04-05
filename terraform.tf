d4c8e4e12b40b0ca7439d153f6fa9ca3b93d3a99
Kerim Satirli <k.satirli@gmail.com>
2023-04-05 14:36:15 +0200

    bumps AWS, HCP, and Gandi providers

--- terraform.tf
+++ terraform.tf
@@ -12,25 +12,25 @@ terraform {
 
   # see https://developer.hashicorp.com/terraform/language/settings#specifying-provider-requirements
   required_providers {
-    # see https://registry.terraform.io/providers/hashicorp/aws/4.53.0/
+    # see https://registry.terraform.io/providers/hashicorp/aws/4.61.0/
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.53.0, < 5.0.0"
+      version = ">= 4.61.0, < 5.0.0"
     }
 
-    # see https://registry.terraform.io/providers/hashicorp/hcp/0.53.0/
+    # see https://registry.terraform.io/providers/hashicorp/hcp/0.56.0/
     hcp = {
       source  = "hashicorp/hcp"
-      version = ">= 0.53.0, < 1.0.0"
+      version = ">= 0.56.0, < 1.0.0"
     }
 
-    # see https://registry.terraform.io/providers/go-gandi/gandi/2.2.2
+    # see https://registry.terraform.io/providers/go-gandi/gandi/2.2.3
     gandi = {
       source  = "go-gandi/gandi"
-      version = ">= 2.2.2, < 3.0.0"
+      version = "2.2.3"
     }
   }
 
   # see https://developer.hashicorp.com/terraform/language/settings#specifying-a-required-terraform-version
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.4.0"
 }
