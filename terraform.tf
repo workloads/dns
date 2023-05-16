3db11e6071a90f2ed5b0219432a37a9100069927
Kerim Satirli <k.satirli@gmail.com>
2023-05-16 13:08:29 +0200

    Updates AWS Provider from `4.65.0` to `4.67.0`

--- terraform.tf
+++ terraform.tf
@@ -12,13 +12,13 @@ terraform {
 
   # see https://developer.hashicorp.com/terraform/language/settings#specifying-provider-requirements
   required_providers {
-    # see https://registry.terraform.io/providers/hashicorp/aws/4.65.0/
+    # see https://registry.terraform.io/providers/hashicorp/aws/4.67.0
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.65.0, < 5.0.0"
+      version = ">= 4.67.0, < 5.0.0"
     }
 
-    # see https://registry.terraform.io/providers/hashicorp/hcp/0.56.0/
+    # see https://registry.terraform.io/providers/hashicorp/hcp/0.56.0
     hcp = {
       source  = "hashicorp/hcp"
       version = ">= 0.56.0, < 1.0.0"
@@ -32,5 +32,5 @@ terraform {
   }
 
   # see https://developer.hashicorp.com/terraform/language/settings#specifying-a-required-terraform-version
-  required_version = ">= 1.4.0"
+  required_version = ">= 1.4.0, < 1.5.0"
 }
