c62a993fd486797304a492898be34779234ddddc
Kerim Satirli <k.satirli@gmail.com>
2023-11-20 09:00:42 +0100

    bumps providers

--- terraform.tf
+++ terraform.tf
@@ -14,16 +14,16 @@ terraform {
 
   # see https://developer.hashicorp.com/terraform/language/settings#specifying-provider-requirements
   required_providers {
-    # see https://registry.terraform.io/providers/hashicorp/aws/5.25.0
+    # see https://registry.terraform.io/providers/hashicorp/aws/5.26.0
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.25.0, < 6.0.0"
+      version = ">= 5.26.0, < 6.0.0"
     }
 
-    # see https://registry.terraform.io/providers/hashicorp/hcp/0.76.0
+    # see https://registry.terraform.io/providers/hashicorp/hcp/0.77.0
     hcp = {
       source  = "hashicorp/hcp"
-      version = ">= 0.76.0, < 1.0.0"
+      version = ">= 0.77.0, < 1.0.0"
     }
 
     # see https://registry.terraform.io/providers/go-gandi/gandi/2.2.4
